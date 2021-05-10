2fea02692dc5e9d47838b778f1fda6fea1c1db87
onth0s <metaagnosis@gmail.com>
2021-05-10 11:35:24 +0200

    kinda fixed the login server call

--- src/services/index.js
+++ src/services/index.js
@@ -11,6 +11,8 @@ export const login = async (userCredentials) => {
 
 	let res = null;
 
+	let requestData = null;
+	
 	try {
 		const res = await axios({
 			method: 'GET',
@@ -22,33 +24,7 @@ export const login = async (userCredentials) => {
 		console.log('Axios Error getting IP:');
 		console.log(err);
 	}
-
-	try {
-		const response = await axios({
-			method: 'POST',
-			url: joinURL(SERVER_URL, 'CMS/login'),
-			data: {
-				userCredentials,
-				IP
-			}
-		});
-
-		// console.log(res);
-
-		// return res;
-		res = response;
-		// return { res, requestData };
-	} catch (err) {
-		console.log('Axios Error logging user in:');
-		console.log(err);
-	}
 	
-	
-	// TODO I tried doing this to measure response time
-	// didn't really work
-	// returns "undefined" no matter what
-
-	let requestData = null;
 	try {
 		axios.interceptors.request.use(request => {
 			// to avoid overwriting if another interceptor
@@ -65,18 +41,41 @@ export const login = async (userCredentials) => {
 			counterLogin++;
 			console.log('counterLogin:', counterLogin);
 			
-			return {
-				test: 'some message'
-			};
+			// return {
+			// 	test: 'some message'
+			// };
 			// return request.meta;
+			
 		})
 	} catch (err) {
 		console.log('Axios Error intercepting request:');
 		console.log(err);
 	}
 	
-	console.log('end of login()');
-	console.log('requestData:');
-	console.log(requestData);
-	return 'test retur222n';
+
+	try {
+		const response = await axios({
+			method: 'POST',
+			url: joinURL(SERVER_URL, 'CMS/login'),
+			data: {
+				userCredentials,
+				IP
+			}
+		});
+
+		console.log('res:');
+		console.log(res);
+
+		// return res;
+		res = response;
+		// return { res, requestData };
+	} catch (err) {
+		console.log('Axios Error logging user in:');
+		console.log(err);
+	}
+	
+	return {
+		res, 
+		requestedAt: requestData.meta.requestStartedAt
+	};
 }
